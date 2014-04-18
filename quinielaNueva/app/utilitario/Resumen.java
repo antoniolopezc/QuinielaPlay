52079adcd47cf10f7d56a4f8bd8a662e8beb5314
antoniolopezc <antonio_lopez_c@hotmail.com>
2014-04-18 11:14:28 -0430

    Crear hoja resumen

---
+++ quinielaNueva/app/utilitario/Resumen.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package utilitario;
+
+/**
+ * @author Antonio Lopez
+ *
+ */
+public class Resumen implements Comparable<Resumen> {
+	
+	Long Posicion;
+	
+	models.Usuario Jugador;
+	
+	Long Punto;
+	
+	Long PorcentajeTotal;
+	
+	Long Maximo;
+	
+	Long Premio;
+	
+	Long TotalMaximo;
+	
+	Boolean Participa;
+	
+	@Override
+	public int compareTo(Resumen o) {
+		return this.Punto.compareTo(o.Punto);
+	}
+
+	public Long getPosicion() {
+		return Posicion;
+	}
+
+	public void setPosicion(Long posicion) {
+		Posicion = posicion;
+	}
+
+	public models.Usuario getJugador() {
+		return Jugador;
+	}
+
+	public void setJugador(models.Usuario jugador) {
+		Jugador = jugador;
+	}
+
+	public Long getPunto() {
+		return Punto;
+	}
+
+	public void setPunto(Long punto) {
+		Punto = punto;
+	}
+
+	public Long getPorcentajeTotal() {
+		return PorcentajeTotal;
+	}
+
+	public void setPorcentajeTotal(Long porcentajeTotal) {
+		PorcentajeTotal = porcentajeTotal;
+	}
+
+	public Long getMaximo() {
+		return Maximo;
+	}
+
+	public void setMaximo(Long maximo) {
+		Maximo = maximo;
+	}
+
+	public Long getPremio() {
+		return Premio;
+	}
+
+	public void setPremio(Long premio) {
+		Premio = premio;
+	}
+
+	public Boolean getParticipa() {
+		return Participa;
+	}
+
+	public void setParticipa(Boolean participa) {
+		Participa = participa;
+	}
+
+	public Long getTotalMaximo() {
+		return TotalMaximo;
+	}
+
+	public void setTotalMaximo(Long totalMaximo) {
+		TotalMaximo = totalMaximo;
+	}
+
+
+}
