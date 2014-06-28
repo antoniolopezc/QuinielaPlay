665fbdc5ec662dd7095f86fc6cbcf5e2d4df2e22
antoniolopezc <antonio_lopez_c@hotmail.com>
2014-06-28 07:28:25 -0430

    Corregir como se ve quien pasa en ver partido

---
+++ quinielaNueva/ver quien falta.sql
@@ -0,0 +1,7 @@
+SELECT rp.pronostico_id,p.nombre , count(*) FROM laquiniela.resultado_pronostico rp left join pronostico p on rp.pronostico_id=p.id
+where resultado_id in (
+ select id from resultado r
+  where r.partido_id between 49 and 56
+) and entero is null 
+  and equipo_id is null
+group by rp.pronostico_id,p.nombre
